fix array scaling bugs at prediction

core/utils.py
@@ -202,7 +202,7 @@ class Utils(object):
         preds = exp_preds / np.sum(exp_preds)
         probas = np.random.multinomial(1, preds, 1)
 
-        return np.argmax(probas)
+        return np.argmax(probas).astype('float32') / 255.
 
 
     @staticmethod

predict_keras_datasets.py
@@ -146,10 +146,10 @@ def predict(argv=None):
                     X_pred[:,i,j,k] = sampled_pred
 
     X_pred = np.squeeze(X_pred)
+    X_pred = (255*X_pred).astype(np.uint8)
 
     ### save images ###
     for i in range(nb_images):
-        print(X_pred[i].shape)
         utils.save_generated_image(X_pred[i], 'generated_'+str(i)+'.jpg', save_path)
 
     return (0)